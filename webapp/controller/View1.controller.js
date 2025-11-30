294e0f3691141f2a40382bf9fde624aa72169563
Nikhil Moharana <vkdk210@gmail.com>
2025-11-30 06:12:49 +0000

    first commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller,JSONModel) => {
+    "use strict";
+
+    return Controller.extend("com.demo.b73ui5app.controller.View1", {
+        onInit() {
+
+            //Read Method (This Method is only use when u need to do some mandatoruy configuration which is not present) *We use JSON model  here
+            //no need but for the understanding purpose i add a SNo. column here ,it works when u bind the data with json modulu configuration ([JSON Model name>/array name (results)])
+            var prdJSONModel = new JSONModel();
+            this.getView().setModel(prdJSONModel,"prdJSONModel");
+
+            var oModel = this.getOwnerComponent().getModel();
+            oModel.read("/ProductSet",{
+                success:function(data){
+                    for(var i=-0;i<data.results.length;i++){
+                        data.results[i].SNo= i+1;
+                    }
+                    prdJSONModel.setData(data);
+                }
+            });
+        }
+    });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/demo/b73ui5app/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
